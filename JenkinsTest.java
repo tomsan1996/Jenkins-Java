ceaf26d7c7620dda173c101df7f4efe623dcf683
tomsan1996 <117666363+tomsan1996@users.noreply.github.com>
2023-02-27 01:11:41 +0700

    Add files via upload

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -1,9 +1,9 @@
-class Main {
+class JenkinsTest {
 
 
     public static void main(String[] args){
 
-        System.out.println("Testing Jenkins Pipeline with Java.")
+        System.out.println("Testing Jenkins Pipeline with Java.");
 
     }
     
